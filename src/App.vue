13d9cb92656dcf28d58ad4d3b77515563918487d
Dimitri Wahyudiputra <gitmitri@gmail.com>
2023-01-25 18:08:39 +0700

    feat: base emit props sample

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,55 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+// App.vue
+//   - foods []
+//   - <Table :foods="foods" />
+
+    // Table.vue
+    //   - props ['foods']
+    //   - <TableRow v-for="food in foods" :food="food" :key="food.id" />
+
+        // TableRow.vue
+        //   - props ['food']
+
+import Card from './components/Card.vue'
+
+export default {
+    // State
+    data() {
+        return {
+            foods: [],
+        }
+    },
+    // Component registration
+    components: {
+        Card,
+    },
+    
+    methods: {
+        deleteFood(id) {
+            console.log('Card menyuruh deleteFood', id)
+        }
+    },
+
+    // Lifecycle
+    // Ketika component sudah berhasil dibuat/render
+    created() {
+        // Fetch food data
+        const foods = [
+            { id: 1, title: 'Ayam goreng', description: 'Ayam goreng enak sekali' },
+            { id: 2, title: 'Burger', description: 'Burger enak' },
+            { id: 3, title: 'Pizza', description: 'Makanan italia' },
+        ]
+
+        this.foods = foods
+    },
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <Card
+        v-for="food in foods"
+        :item="food"
+        @deleteFood="deleteFood" />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style></style>
---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    // Card menerima data title dan description
+    props: ['item']
+}
+</script>
+
+<template>
+    <div class="card">
+        <h1>{{ item.title }}</h1>
+        <p>{{ item.description }}</p>
+        <button @click="$emit('deleteFood', item.id)">Delete</button>
+    </div>
+</template>
+
+<style>
+.card {
+    padding: 16px;
+    border: 1px solid #f0f0f0;
+}
+</style>
